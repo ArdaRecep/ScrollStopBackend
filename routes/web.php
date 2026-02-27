ed0759cb753fc303a74ed93c1d46e0872e123923
ArdaRecep07 <aarrddaa415@gmail.com>
2026-02-27 12:03:01 +0300

    feat: init laravel

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/health', fn () => response()->json([
+  'ok' => true,
+  'service' => 'scrollstop-backend',
+]));
